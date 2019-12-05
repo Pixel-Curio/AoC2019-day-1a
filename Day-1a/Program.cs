62ea188b4d62e7fa92c92d4aba02b030b8b886b3
Michael Sanders <mike.gamedev@gmail.com>
2019-12-05 13:32:53 -0800

    Initial commit.

---
+++ Day-1a/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1a
+{
+    class Program
+    {
+        private const string InputPath = @"day1a-input.txt";
+
+        static void Main(string[] args)
+        {
+            string[] inputs = File.ReadAllLines(InputPath);
+            Console.WriteLine(inputs.Select(int.Parse).Select(x => x/3-2).Sum());
+            Console.ReadKey();
+        }
+    }
+}
